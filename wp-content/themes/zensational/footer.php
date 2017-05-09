6c6974a7c488aa5c65e18a603ac66efc7f1399c3
Ducraisse Mabika <ducraisse@Ducraisses-iMac.local>
2017-05-09 22:55:30 +0100

    footer mobile styles

--- wp-content/themes/zensational/footer.php
+++ wp-content/themes/zensational/footer.php
@@ -1,3 +1,91 @@
+<footer class="footer">
+    <div class="footer__subscribe">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="subscribe">
+                        <label for="sign-up">Sign up for our mailing list</label>
+                        <div class="subscribe__holder">
+                            <input placeholder="Email" name="sign-up">
+                            <button type="submit">Sign up</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer__info">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-4">
+                    <div class="footer__info__item">
+                        <h2>Find Us</h2>
+                        <p>
+                            Zensational Skin & Beauty Therapy <br>
+                            69 Lever Street <br>
+                            Northern Quarter <br>
+                            Manchester <br>
+                            M1 1FL
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-2">
+                    <div class="footer__info__item">
+                        <h2>Useful Information</h2>
+                        <ul>
+                            <li>
+                                <a href="#" title="about us">About Us</a>
+                            </li>
+                            <li>
+                                <a href="#" title="services">Services</a>
+                            </li>
+                            <li>
+                                <a href="#" title="blog">Blog</a>
+                            </li>
+                            <li>
+                                <a href="#" title="contact us">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-4">
+                    <div class="footer__info__item">
+                        <h2>Contact Us</h2>
+                        <p>Call Us: <a href="tel:01612627841">01612627841</a></p>
+                        <p>Email Us: <a href="mailto:info@zensational.co.uk">info@zensational.co.uk</a></p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-2">
+                    <div class="footer__info__item">
+                        <h2>Follow Us</h2>
+                        <ul class="footer-social">
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-instagram" aria-hidden="true"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-twitter"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-facebook"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer__copyright">
+        <p>© Copyright 2016 Zensational Skin and Beauty Therapy</p>
+        <p>Site build by Dayfive Digital </p>
+    </div>
+</footer>
+
 <script defer="defer" src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/dayfive.js"></script>
 <?php wp_footer(); ?>
 </div> <!-- closing row -->
